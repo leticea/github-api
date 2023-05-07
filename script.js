3d33f2ff5da22828b70d85fd1fafccaf4634964c
leticea <nascimento.leticeah@gmail.com>
2023-05-07 16:04:28 -0300

    feat(script.js): main js codes

--- script.js
+++ script.js
@@ -1,4 +1,7 @@
 const searchGithub = async () => {
   const username = document.getElementById("searchInput");
+  const response = await fetch(`https://api.github.com/users/${username}`);
+  const detailsContainer = document.querySelector(".details");
+  const data = await response.json();
 }
 
