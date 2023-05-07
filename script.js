84e583b9906a40efdf001c37f44897bfa13cb316
leticea <nascimento.leticeah@gmail.com>
2023-05-07 16:13:13 -0300

    feat(script.js): showing result

--- script.js
+++ script.js
@@ -3,5 +3,19 @@ const searchGithub = async () => {
   const response = await fetch(`https://api.github.com/users/${username}`);
   const detailsContainer = document.querySelector(".details");
   const data = await response.json();
-}
 
+  if (response.success) {
+    detailsContainer.style.display = "flex";
+    document.getElementById("result").innerHTML = `
+      <div class="profile">
+        <div class="profile-image">
+          <img src="${data.avatar_url}" />
+        </div>
+        <div class="profile-details">
+          <h2 class="name">${data.name || data.login}</h2>
+          <p class="username">@${data.login}</p>
+        </div>
+      </div>
+    `;
+  }
+};
